7f37874b27de68f3ba22801943228ae1630b023d
Hyeonsik Song <blogcin@naver.com>
2018-08-29 17:43:35 +0900

    액션 레코드 추가
    Signed-off-by: Hyeonsik Song <blogcin@naver.com>

--- handler.c
+++ handler.c
@@ -23,3 +23,23 @@ cleanup:
 
   return U_CALLBACK_CONTINUE;
 }
+
+int handler_record(const struct _u_request *request, struct _u_response *response, void *user_data) {
+  char *res = NULL;
+  char *command = NULL;
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+
+  asprintf(&res, "{ status : 1 }");
+  asprintf(&command, "ffmpeg -i http://127.0.0.1:9980/webcam.mpg -c copy -t 10 %d_%d_%d_%d_%d_%d.mpg", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+  system(command);
+
+cleanup:
+  if (res) {
+    ulfius_set_string_body_response(response, 200, res);
+    free(res);
+  }
+
+  return U_CALLBACK_CONTINUE;
+}
